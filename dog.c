195e2d8c0a7f08c7f37fb53db0b582bf7860cc85
Arvonit <me@arvind.dev>
2020-09-06 23:00:50 -0400

    Begin support for reading multiple files at a time

--- dog.c
+++ dog.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
     FILE *file;
-    char *file_name = argv[1];
+    char *file_name;
     char file_char;
 
-    if (argc != 2) {
+    // Must include one file name to continue
+    if (argc < 2) {
         fprintf(stderr, "usage: dog [file]\n");
         exit(EXIT_FAILURE);
     }
 
-    file = fopen(file_name, "r");
-    while ((file_char = fgetc(file)) != EOF) {
-        printf("%c", file_char);
+    // Iterate through arguments
+    for (int i = 1; i < argc; i++) {
+        file_name = argv[i];
+
+        file = fopen(file_name, "r");
+        if (file) {
+            while ((file_char = fgetc(file)) != EOF) {
+                printf("%c", file_char);
+            }
+        } else {
+            fprintf(stderr, "dog: %s: No such file or directory\n", file_name);
+        }
+        fclose(file);
     }
-    fclose(file);
-    
-    exit(EXIT_SUCCESS);
 }
